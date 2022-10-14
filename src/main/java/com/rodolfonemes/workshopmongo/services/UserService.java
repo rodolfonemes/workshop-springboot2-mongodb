ba66daa43ecf8154b004f900b4cf4665b9864c76
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-10-13 21:00:20 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/rodolfonemes/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.rodolfonemes.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodolfonemes.workshopmongo.domain.User;
+import com.rodolfonemes.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
